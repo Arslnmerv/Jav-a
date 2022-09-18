45cfc292ded9ca60584e30bd374fa470fea701ed
Arslnmrv <arslanmerve1619@gmail.com>
2022-09-18 22:07:37 +0300

    d/1

---
+++ src/main/java/day01variables/Variables01.java
@@ -0,0 +1,61 @@
+package day01variables;
+
+public class Variables01 {
+
+    // Variable nasil olusturulur?
+
+/*
+    1.) Access Modifier
+    2.)Data Type
+    3.)Variable ismi
+    4.) =
+    5.)Deger
+    6.);
+ */
+
+    // Java'da ";" ingilizcedeki "." gibidir. Ingilizcede "cumle" deriz, javada "statement" deriz.
+    // "=" --> Assigment (Atama) Operaror sagdaki degeri alir soldaki variable'in icine koyar.
+
+    public int age = 13;
+
+    public int height = 183;
+
+    // Java'da Data Type'lar
+        /*
+          1.) int: Integer'in kisaltmasi. Integer tam sayi demektir.
+               Matematik'te tam sayilarin basi ve sonu yoktur fakat Java'da tam sayilarin basi ve sonu vardir.
+               En kucuk int = -2,147,483,648
+               En buyuk int = 2,147,483,647
+
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
